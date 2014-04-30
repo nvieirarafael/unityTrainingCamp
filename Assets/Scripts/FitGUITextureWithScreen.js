43ebeb74d6ba50db27f798e1ad5dcb7110c98e01
Rafael Nunes Vieira <nvieirarafael@gmail.com>
2014-04-30 13:24:14 -0300

    added some terrain using Ferr plugin // add a Background that fits with the screen

---
+++ Assets/Scripts/FitGUITextureWithScreen.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+  var gui : GUITexture = GetComponent(GUITexture);
+  var screenWidth : float = Screen.width + 4;
+  var screenHeight : float = Screen.height + 4;
+
+  gui.pixelInset.width = screenWidth;
+  gui.pixelInset.height = screenHeight;
+
+  gui.pixelInset.x = 0;
+  gui.pixelInset.y = 0;
+}
